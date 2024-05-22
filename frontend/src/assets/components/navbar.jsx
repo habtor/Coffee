6679db5e860fb5fc1d9f877c11ec3c08116b5045
Saleh <39927806+habtor@users.noreply.github.com>
2024-05-22 21:28:43 +0200

    added tailwind

---
+++ frontend/src/assets/components/navbar.jsx
@@ -0,0 +1,17 @@
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <img src="" alt="" />
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
