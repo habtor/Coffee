a9236aadd6180a1fd9eef374c27b2a9179c612ee
Saleh <39927806+habtor@users.noreply.github.com>
2024-07-28 18:09:01 +0200

    added search bar

--- frontend/src/pages/shop.jsx
+++ frontend/src/pages/shop.jsx
@@ -1,8 +1,10 @@
 import BeansStore from "../components/shop/beans.jsx";
+import SearchBar from "../components/shop/searchBar.jsx";
 
 function Shop() {
   return (
-    <div>
+    <div className="relative sm:w-full w-[306px] max-w-[1200px] m-auto mt-10">
+      <SearchBar />
       <BeansStore />
     </div>
   );
